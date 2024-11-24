f2339ed62a471179c1285b7d15d0bb24adad7afc
RadCon Max <RadConMax@gmail.com>
2024-11-24 00:34:11 -0800

    Fixing markdown-preview build on windows

--- lua/plugins/tools/markdown.lua
+++ lua/plugins/tools/markdown.lua
@@ -7,8 +7,13 @@ return {
         {
             'iamcco/markdown-preview.nvim',
             cmd = { 'MarkdownPreviewToggle', 'MarkdownPreview', 'MarkdownPreviewStop' },
-            build = function()
-                vim.fn['mkdp#util#install']()
+            build = function(plugin)
+                if vim.fn.executable "npx" then
+                    vim.cmd("!cd " .. plugin.dir .. " && cd app && npx --yes yarn install")
+                else
+                    vim.cmd [[Lazy load markdown-preview.nvim]]
+                    vim.fn["mkdp#util#install"]()
+                end
             end,
         },
     },
