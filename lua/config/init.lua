4469ad47c78b91bd2cea8f4ae184ca57ec9b2bc9
Jesus Barajas <135153060+jesus-barajas@users.noreply.github.com>
2025-11-13 13:06:05 -0800

    Moving mappings into which_key.lua file

--- lua/config/init.lua
+++ lua/config/init.lua
@@ -1,3 +1,2 @@
 require('config.settings')
 require('config.lazy')
-require('config.mappings')
--- lua/plugins/ui/which_key.lua
+++ lua/plugins/ui/which_key.lua
@@ -31,6 +31,53 @@ return {
         which_key.add({
             hidden = true,
             { '<leader>kk', ':WhichKey<cr>', desc = 'Which Key (Open)' },
+            {
+                -- Text edit
+                { 'J', 'mzJ`z', desc = 'Append next line to current line', icon = '' },
+                { 'N', 'Nzzzv', desc = 'Search next but keep cursor centered', icon = '' },
+                -- Navigation
+                { '<c-d>', '<C-d>zz', desc = 'Move half page down but keep cursor centered', icon = '󰮱' },
+                { '<c-u>', '<C-u>zz', desc = 'Move half page up but keep cursor centered', icon = '󰮳' },
+                { '<leader>cc', ':qa<cr>', desc = 'Exit', icon = '󱎘' },
+                { '<leader>hh', ':split<cr>', desc = 'Horizontal split', icon = '' },
+                { '<leader>qq', ':q<cr>', desc = 'Buffer (Close)', icon = '󱎘' },
+                { '<leader>QQ', ':q!<cr>', desc = 'Buffer (Force Close)', icon = '󱎘' },
+                { '<leader>rr', ':so<cr>', desc = 'Source current file' },
+                { '<leader>ss', ':w<cr>', desc = 'Save', icon = '' },
+                { '<leader>SS', ':w!<cr>', desc = 'Save (Force)', icon = '' },
+                { '<leader>xx', ':x<cr>', desc = 'Save and close (if modified)', icon = ' 󱎘' },
+                { '<leader>vv', ':vsplit<cr>', desc = 'Vertical split', icon = '' },
+                { '<leader>VV', 'ggVG', desc = 'Select all', icon = '󰒆' },
+                { '<leader>ww', ':windo diffthis<cr>', desc = 'Diff', icon = '' },
+                { '<Esc>', ':noh<cr>', desc = 'Close highlights', icon = '󱎘' },
+                { '[q', ':cp<cr>', desc = 'Previous quickfix item', icon = '' },
+                { ']q', ':cn<cr>', desc = 'Next quickfix item', icon = '' },
+                { 'n', 'nzzzv', desc = 'Search previous but keep cursor centered', icon = '' },
+            },
+            {
+                -- Tools
+                { '<leader>ll', ':Lazy<cr>', desc = 'Lazy (Open)', icon = '󰀻' },
+                { '<leader>T', ':terminal<cr>', desc = 'Terminal (Open)', icon = '' },
+            },
+        })
+        which_key.add({
+            -- Set filetype
+            { '<leader>F', desc = 'Filetype', icon = '' },
+            { '<leader>Fc', ':set filetype=config<cr>', desc = 'Config', icon = '' },
+            { '<leader>Fj', ':set filetype=json<cr>', desc = 'JSON', icon = '' },
+            { '<leader>Fl', ':set filetype=lua<cr>', desc = 'Lua', icon = '' },
+            { '<leader>Fm', ':set filetype=markdown<cr>', desc = 'Markdown', icon = '' },
+            { '<leader>Fp', ':set filetype=python<cr>', desc = 'Python', icon = '' },
+            { '<leader>Fq', ':set filetype=sql<cr>', desc = 'SQL', icon = '' },
+            { '<leader>Fs', ':set filetype=sh<cr>', desc = 'Shell', icon = '' },
+            { '<leader>Ft', ':set filetype=typescript<cr>', desc = 'TypeScript', icon = '' },
+            { '<leader>Fv', ':set filetype=javascript<cr>', desc = 'JavaScript', icon = '' },
+        })
+        which_key.add({
+            mode = { 'v' },
+            -- Text edit
+            { 'J', ':m \'>+1<CR>gv=gv', desc = 'Move selected line up', icon = '' },
+            { 'K', ':m \'<-2<CR>gv=gv', desc = 'Move selected line down', icon = '' },
         })
     end,
 }
